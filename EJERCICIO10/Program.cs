be48d3802b9086b27e7f6b48023c433858ce257a
alexvc19111 <118561767+alexvc19111@users.noreply.github.com>
2022-11-19 17:50:33 -0500

    Add files via upload

---
+++ EJERCICIO10/Program.cs
@@ -0,0 +1,15 @@
+﻿class Ejercicio10
+{
+    static void Main(string[] args)
+    {
+        int fac=1;
+        Console.WriteLine("Ingrese el número entero del cual quiere calcular su factorial :");
+        int n=Convert.ToInt32(Console.ReadLine());
+        int n2=n;
+        for(int x=1;x<=n;x++)
+        {
+            fac=x*fac;
+        }
+        Console.WriteLine("El factorial de "+n+" es igual a :"+fac);
+    }
+}
---
+++ EJERCICIO9/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        float suma=0;
+        float x2=5;
+        for(int x=0;x<=5;x++)
+        {
+            float x3=x2-x;
+            suma=suma+x3;
+        }
+        float media=suma/x2;
+        Console.WriteLine("Media aritmetica es igual a :"+media);
+    }
+}
